a311c72020b100d475a6b504ef20d492a350d370
Divya <dsrao0712@gmail.com>
2021-11-02 23:39:15 +0530

    display query params on console

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
+const url = require('url')
 const express = require('express')
 const router = express.Router()
+const needle = require('needle')
 
-router.get('/', (req, res) => {
-    res.json({ 'success': true })
+
+// Environment variables 
+
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
+
+router.get('/', async(req, res) => {
+
+    try {
+
+        console.log("Fetching data from API ...")
+
+        const params = new URLSearchParams({
+            [API_KEY_NAME]: API_KEY_NAME,
+            ...url.parse(req.url, true).query
+        })
+
+        const apiRes = await needle('get', `${API_BASE}?${params}`)
+        const data = apiRes.body
+        console.log(data)
+        res.status(200).json(data)
+
+
+    } catch (error) {
+
+        res.status(500).json(error)
+
+    }
+
 })
 
 module.exports = router
