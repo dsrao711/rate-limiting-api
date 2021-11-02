c32cc40eb90bfde143769d0089b3253379835cf7
Divya <dsrao0712@gmail.com>
2021-11-03 02:30:22 +0530

    completed

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const url = require('url')
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
+const apicache = require('apicache')
 
 
 // Environment variables 
@@ -10,25 +11,33 @@ const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
 
-router.get('/', async(req, res) => {
+// init cache
+
+let cache = apicache.middleware
+
+
+router.get('/', cache('2 minutes'), async(req, res) => {
 
     try {
-
-        console.log("Fetching data from API ...")
-
+        // Query params
         const params = new URLSearchParams({
-            [API_KEY_NAME]: API_KEY_NAME,
-            ...url.parse(req.url, true).query
-        })
-
-        const apiRes = await needle('get', `${API_BASE}?${params}`)
+                [API_KEY_NAME]: API_KEY_VALUE,
+                ...url.parse(req.url, true).query,
+            })
+            // URL with the base url , api key and the query
+        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
         const data = apiRes.body
-        console.log(data)
+
+        // Log the request to the public API
+        if (process.env.NODE_ENV !== 'production') {
+            console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+        }
+
+        // Display data on success
         res.status(200).json(data)
 
-
     } catch (error) {
-
+        // Display error
         res.status(500).json(error)
 
     }
